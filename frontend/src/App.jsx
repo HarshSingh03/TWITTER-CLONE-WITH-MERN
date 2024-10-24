dcd1545ffa61c3c61ee6e5b1e75ce3f82a9d0ea4
HARSH SINGH <harshsingh.bar@gmail.com>
2024-10-24 13:24:35 +0530

    Front End 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-
+import {Route, Routes} from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <button className="btn btn-outline">Default</button>
-<button className="btn btn-outline btn-primary">Primary</button>
-<button className="btn btn-outline btn-secondary">Secondary</button>
-<button className="btn btn-outline btn-accent">Accent</button>
-    </>
+
+      <div className='flex max-w-6xl mx-auto '>
+        {/*Commpn component, bc it's not wrapped with Routes*/}
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/signup' element={<SignUpPage/>}/>
+        </Routes>
+        <RightPanel />
+      </div>
   )
 }
 
-export default App
+export default App;
