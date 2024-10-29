9bf19528967f924d6f6dede16f433e9811f8db0a
HARSH SINGH <harshsingh.bar@gmail.com>
2024-10-29 16:03:27 +0530

    like post mutation added

--- backend/controllers/post_controller.js
+++ backend/controllers/post_controller.js
@@ -94,7 +94,8 @@ export const likeUnlikePost = async (req,res) => {
       //Unlike Post
       await Post.updateOne({_id:postId},{$pull:{likes:userId}});
       await User.updateOne({_id:userId},{$pull:{likedPosts:postId}});
-      res.status(200).json({message:"Post unliked successfully"});
+      const updatedLikes = post.likes.filter((id)=> id.toString()!== userId.toString())
+      res.status(200).json(updatedLikes);
     }
     else{
       post.likes.push(userId);
@@ -108,7 +109,8 @@ export const likeUnlikePost = async (req,res) => {
       });
 
       await notification.save();
-      res.status(200).json({message:"Post liked successfully"});
+      const updatedLikes = post.likes;
+      res.status(200).json(updatedLikes);
     }
 
   }
