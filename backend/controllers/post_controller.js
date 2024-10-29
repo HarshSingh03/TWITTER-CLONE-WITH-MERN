3c7347ecd31b251779b7bca829b67ee055482815
HARSH SINGH <harshsingh.bar@gmail.com>
2024-10-29 19:48:03 +0530

    comment of post mutation added

--- backend/controllers/post_controller.js
+++ backend/controllers/post_controller.js
@@ -73,7 +73,7 @@ export const commentOnPost = async (req,res) => {
     post.comments.push(comment);
     
     await post.save();
-    res.status(200).json(post);
+    res.status(200).json(post.comments);
     
   } catch (error) {
     console.log("Error in commentOnPost",error);
