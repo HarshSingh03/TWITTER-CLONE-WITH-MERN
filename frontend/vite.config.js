134e13e8a8299a74d9055bab3bc0d4d419d99e6a
Harsh Singh <148679444+HarshSingh03@users.noreply.github.com>
2025-01-23 07:13:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+/*  server:{
     port:3000,
     proxy:{
       "/api":{
@@ -12,5 +12,5 @@ export default defineConfig({
         changeOrigin:true
       }
     }
-  }
+  }*/
 })
